eea8c05df9ac466c278524873f1851fa553b9317
Leon Xu <xfstart07@gmail.com>
2018-04-09 18:16:24 +0800

    fix exp log

--- example/crontab.go
+++ example/crontab.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"gocrontab"
-	"fmt"
+	"github.com/xfstart07/gocrontab"
+	log "github.com/xfstart07/logger"
 	"time"
 )
 
 func main() {
 	scheduler := gocrontab.NewSchedule()
 	scheduler.NewJob("test1").Every(10).Seconds().Do(func() {
-		fmt.Println("test1......")
+		log.Println("test1......")
 	})
 
 	scheduler.Start()
 
 	scheduler.NewJob("test2").Every(15).Seconds().Do(func() {
-		fmt.Println("test2.....")
+		log.Println("test2.....")
 	})
 
-	fmt.Println(scheduler.Entries())
+	log.Println(scheduler.Entries())
 	for {
 		time.Sleep(15 * time.Second)
 
@@ -27,21 +27,22 @@ func main() {
 		break
 	}
 
-	fmt.Println("删除", scheduler.RemoveJob("test2"))
-	fmt.Println("删除", scheduler.RemoveJob("test2"))
+	log.Println("删除", scheduler.RemoveJob("test2"))
+	log.Println("删除", scheduler.RemoveJob("test2"))
 
 	scheduler.NewJob("testc").Every(15).Minutes().Do(func() {
-		fmt.Println("test2.....")
+		log.Println("test2.....")
 	})
 
 	scheduler.NewJob("testb").Every(1).Days().At(12, 0).Do(func() {
-		fmt.Println("testb...")
+		log.Println("testb...")
 	})
 
 	entries := scheduler.Entries()
+	log.Println(scheduler.Entries())
 	for idx := range entries {
-		fmt.Printf("%+v\n", entries[idx])
+		log.Printf("%+v\n", entries[idx])
 	}
-	fmt.Println(scheduler.Entries())
+	log.Println(scheduler.Entries())
 }
 
